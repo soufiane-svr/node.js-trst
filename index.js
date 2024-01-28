4383c0423a822f9fcea7b85785f4b8db305dd7a3
soufiane-svr <nwigashop@gmail.com>
2024-01-28 02:07:11 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,73 @@
+const express= require('express')
+const app = express()
+app.use(express.json())
+const Article = require ('./models/Article.js')
+const mongoose = require('mongoose')
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
+
+//ربط الموقع بقاعدة البيانات
+mongoose.connect("mongodb+srv://Sousou:Sousou12@cluster0.3ftiagc.mongodb.net/?retryWrites=true&w=majority").then(()=>{
+    console.log('connected is saccussfully')
+}).catch(()=>{
+    console.log('error');
+})
+//Send and save information for database and create new poste in postman 
+app.post('/article', async(req,res)=>{
+   const newArticle = new Article()
+   newArticle.title = req.body.name;
+   newArticle.body =req.body.city;
+   newArticle.numberOfLike = req.body.number ;
+   await newArticle.save()   
+
+  res.send('the new article module is has been store')
+
+})
+//Get information for database ('it's perfect id')
+app.get('/article',async(req,res)=>{
+    const Database = await Article.find()
+    res.json(Database) 
+})
+
+
+//test requise of postmain
+app.post('/posted',async(req,res)=>{
+    art = new Article
+    art.title = req.body.name
+    art.body = req.body.city
+    art.numberOfLike = req.body.number
+     art.save()
+    
+     await  res.send('hello world')
+})
+//
+
+
+
+
+//mongodb+srv://Sousou:<password>@cluster0.3ftiagc.mongodb.net/?retryWrites=true&w=majority
+
+
+app.get('/ab',async(req,res)=>{
+    art =await Article.find()
+    res.render('index.ejs',{
+        arab : art
+    })
+})
+
+//get HTML page
+
+//detete post in database
+app.delete('/getid/:articleid',async(req,res)=>{
+    id = req.params.articleid
+    post = await Article.findByIdAndDelete(id)
+    res.json(post)
+})
+
+
+app.listen(1000,()=>{
+    console.log('hello soufiane')
+})
+ 
---
+++ models/Article.js
@@ -0,0 +1,13 @@
+const mongoose = require ('mongoose')
+const Schema = mongoose.Schema
+
+const articleSchema = new Schema({
+    title : String,
+    body : String,
+    numberOfLike : Number
+})
+
+
+const Article = mongoose.model('Article',articleSchema)
+
+module.exports = Article
